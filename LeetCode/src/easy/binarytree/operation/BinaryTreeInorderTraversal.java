5493b1af1fe5dff38c24bfc212cc35b4d63a9fe4
Qing Wang <wangqingemail@gmail.com>
2015-08-10 18:35:24 -0400

    Add some test cases for inorder traversal

--- LeetCode/src/easy/binarytree/operation/BinaryTreeInorderTraversal.java
+++ LeetCode/src/easy/binarytree/operation/BinaryTreeInorderTraversal.java
@@ -20,4 +20,22 @@ public class BinaryTreeInorderTraversal {
 		}
 		return treeNodes;
 	}
+	
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(5);
+		TreeNode l1 = new TreeNode(1);
+		TreeNode r1 = new TreeNode(6);
+		root.left = l1;
+		root.right = r1;
+		
+		TreeNode l2 = new TreeNode(0);
+		TreeNode r2 = new TreeNode(10);
+		l1.left = l2;
+		l1.right = r2;
+		
+		BinaryTreeInorderTraversal bt = new BinaryTreeInorderTraversal();
+		List<Integer> result = bt.inorderTraversal(root);
+		System.out.println(result);
+		
+	}
 }
