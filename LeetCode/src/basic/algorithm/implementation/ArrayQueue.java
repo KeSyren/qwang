160d161a540e0845296136b0b75a0851ca256ce6
Qing Wang <wangqingemail@gmail.com>
2021-01-22 14:22:46 -0500

    array to queue

--- LeetCode/src/basic/algorithm/implementation/ArrayQueue.java
+++ LeetCode/src/basic/algorithm/implementation/ArrayQueue.java
@@ -1,5 +1,62 @@
 package basic.algorithm.implementation;
 
 public class ArrayQueue {
+	public Integer[] arr;
+	private Integer size;
+	private Integer start;
+	private Integer end;
 
+	public ArrayQueue(int initSize) {
+		if (initSize < 0) {
+			throw new IllegalArgumentException("This init size is less than 0");
+		}
+
+		arr = new Integer[initSize];
+		size = 0;
+		start = 0;
+		end = 0;
+	}
+
+	public Integer peek() {
+		if (size == 0) {
+			return null;
+		}
+
+		return arr[start];
+	}
+
+	public void push(int obj) {
+		if (size == arr.length) {
+			throw new IllegalArgumentException("The queue is full");
+		}
+		size++;
+		arr[end] = obj;
+		end = (end == arr.length - 1) ? 0 : end+1;
+	}
+
+	public Integer poll() {
+		if (size == 0) {
+			throw new IllegalArgumentException("The queue is empty");
+
+		}
+		size--;
+		int tmp = start;
+		start = (start == arr.length - 1) ? 0 : start + 1;
+		return arr[tmp];
+	}
+	
+	public void print(Integer[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i].toString() + " ");
+		}
+	}
+ 	
+	
+	public static void main(String args[]) {
+		ArrayQueue q = new ArrayQueue(3);
+		q.push(10);	
+		q.push(20);
+		q.push(30);
+		System.out.println(q.poll() + " Popped from queue");
+	}
 }
--- LeetCode/src/basic/algorithm/implementation/ArrayStack.java
+++ LeetCode/src/basic/algorithm/implementation/ArrayStack.java
@@ -12,47 +12,49 @@ public class ArrayStack {
 
 	/**
 	 * Constructor
+	 * 
 	 * @param initSize
 	 */
 	public ArrayStack(int initSize) {
-		if (initSize < 0) throw new IllegalArgumentException("This init size is less than 0.");
-		arr =  new Integer[initSize];
+		if (initSize < 0)
+			throw new IllegalArgumentException("This init size is less than 0.");
+		arr = new Integer[initSize];
 		index = 0;
 	}
-	
+
 	/**
 	 * return top value.
+	 * 
 	 * @return
 	 */
 	public Integer peek() {
 		if (index == 0) {
 			return null;
 		}
-		return arr[index -1];
+		return arr[index - 1];
 	}
-	
-	public void push (int obj) {
+
+	public void push(int obj) {
 		if (index == arr.length) {
 			throw new ArrayIndexOutOfBoundsException("This queue is full.");
 		}
 		arr[index++] = obj;
 	}
-	
+
 	public Integer pop() {
 		if (index == 0) {
 			throw new ArrayIndexOutOfBoundsException("This queue is empty.");
 		}
-		
+
 		return arr[--index];
 	}
-	
-	public static void main(String args[])
-    {
-        ArrayStack s = new ArrayStack(3);
-        s.push(10);
-        s.push(20);
-        s.push(30);
-        System.out.println(s.pop() + " Popped from stack");
-    }
+
+	public static void main(String args[]) {
+		ArrayStack s = new ArrayStack(3);
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.pop() + " Popped from stack");
+	}
 
 }
