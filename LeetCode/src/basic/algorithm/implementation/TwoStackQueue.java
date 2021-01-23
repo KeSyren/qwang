cf7715b8618687590347d7e1eae2be3618c1408c
Qing Wang <wangqingemail@gmail.com>
2021-01-22 22:19:38 -0500

    two stack queue

--- LeetCode/src/basic/algorithm/implementation/TwoStackQueue.java
+++ LeetCode/src/basic/algorithm/implementation/TwoStackQueue.java
@@ -6,48 +6,46 @@ public class TwoStackQueue {
 
 	Stack<Integer> stackPush;
 	Stack<Integer> stackPop;
-	
+
 	public TwoStackQueue() {
 		this.stackPop = new Stack<Integer>();
 		this.stackPush = new Stack<Integer>();
 	}
-	
+
 	public void push(int pushInt) {
 		stackPush.push(pushInt);
 	}
-	
+
 	public int poll() {
-		if(stackPop.empty() && stackPush.empty()) {
+		if (stackPop.empty() && stackPush.empty()) {
 			throw new RuntimeException("Queue is empty");
-			
+
 		} else if (stackPop.empty()) {
 			while (!stackPush.empty()) {
 				stackPop.push(stackPush.pop());
 			}
-		} 
+		}
 		return stackPop.pop();
 	}
-	
+
 	public int peek() {
-		if(stackPop.empty() && stackPush.empty()) {
+		if (stackPop.empty() && stackPush.empty()) {
 			throw new RuntimeException("Queue is empty");
-			
+
 		} else if (stackPop.empty()) {
 			while (!stackPush.empty()) {
 				stackPop.push(stackPush.pop());
 			}
-		} 
+		}
 		return stackPop.peek();
 	}
-	
-	
+
 	public static void main(String[] args) {
-			TwoStackQueue sq = new TwoStackQueue();
-			sq.push(4);
-			sq.push(2);
-			sq.push(12);
-			System.out.print(sq.poll());
-		}
-	
-	
+		TwoStackQueue sq = new TwoStackQueue();
+		sq.push(4);
+		sq.push(2);
+		sq.push(12);
+		System.out.print(sq.poll());
+	}
+
 }
