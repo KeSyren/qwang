ddcd5724c11d764490919087575169eded6795f0
Qing Wang <wangqingemail@gmail.com>
2021-01-23 23:56:26 -0500

    linked list operation

---
+++ LeetCode/src/basic/algorithm/linkedlist/IsPalindrome.java
@@ -0,0 +1,20 @@
+package basic.algorithm.linkedlist;
+
+public class IsPalindrome {
+
+	class Node {
+		public int value;
+		public Node next;
+		
+		public Node(int value) {
+			this.value = value;
+		}
+	}
+	
+	// needs n extra space
+	public static boolean isPalindrome1(Node head) {
+		return false;
+	}
+	
+	
+}
